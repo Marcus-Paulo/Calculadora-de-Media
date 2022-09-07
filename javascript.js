d8747c180ded19b793e6ba776d9288a54dd66ffd
Marcus Costa <mp-oliver@hotmail.com>
2022-09-07 13:33:05 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+var nome = "Marcus"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 7
+var notaDoTerceiroBimestre = 4
+var notaDoQuartoBimestre = 2
+
+
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+    
+console.log ("Bem-Vindo " + nome)
+console.log (notaFinal)
+
+//Isso é um comentário
+
+//Revisão
+//Variáveis, string, console.log, tofixed, operações matemáticas, concatenação
